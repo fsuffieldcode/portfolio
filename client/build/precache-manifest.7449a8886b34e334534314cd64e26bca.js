35f1b615198099208475edf5b679ba8a21bba2e8
fsuffieldcode <fsuffieldcode@gmail.com>
2022-09-26 13:19:43 +0100

    file fix

---
+++ client/build/precache-manifest.7449a8886b34e334534314cd64e26bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15752e71741e0282bcf90dde77416c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849e9e5b3e5e21bc9098",
+    "url": "/static/js/2.4bb35618.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.4bb35618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113fba8655bc08545bbe",
+    "url": "/static/js/main.ebb00025.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
